4ce8a4b780cb97c0f49c6dcdd5d08fc13c47de1b
Abhik4004 <ghos.abhik.2004@gmail.com>
2024-07-21 19:19:14 +0530

    Front-end Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 } from "uuid";
 
 const tasksRSVP = [
   {
@@ -48,11 +49,24 @@ const tasksRSVP = [
 ];
 
 export default function App() {
+  let rsvpList = tasksRSVP;
+
   const [showForm, setShowForm] = useState(false);
+  const [tasks, setTasks] = useState(rsvpList);
+  const [showRSVP, setShowRSVP] = useState(false);
+
   function handleForm() {
     setShowForm((form) => !form);
-    console.log(showForm);
   }
+
+  function formRSVP() {
+    setShowRSVP((form) => !form);
+  }
+
+  function handleEvent(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
   return (
     <div
       className="container"
@@ -63,22 +77,31 @@ export default function App() {
         padding: 0,
       }}
     >
-      <h1 className="title">Task RSVP App</h1>
-      <div>
-        <Tasks />
+      <div className="heading">
+        <h1 className="title">Task RSVP App</h1>
       </div>
-      {showForm && <AddEvent />}
-      <button className="Add-Event" onClick={handleForm}>
-        {showForm ? "Close" : "Add"}
-      </button>
+      <div>
+        <Tasks rsvpList={tasks} />
+      </div>
+      <div className="buttons">
+        {showForm && <AddEvent newEvent={handleEvent} showForm={setShowForm} />}
+        <button className="Add-Event" onClick={handleForm}>
+          {showForm ? "Close" : "Add"}
+        </button>
+        {showRSVP && <AddRSVP showRSVP={setShowRSVP} rsvp={rsvpList} />}
+        <button className="add-rsvps" onClick={formRSVP}>
+          {showRSVP ? "Close" : "Add RSVP(s)"}
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
 
-function Tasks() {
+function Tasks({ rsvpList }) {
   return (
     <div>
-      {tasksRSVP.map((task) => (
+      {rsvpList.map((task) => (
         <RSVP task={task} key={task.id} />
       ))}
     </div>
@@ -93,14 +116,12 @@ function RSVP({ task }) {
   }
   return (
     <div className="rsvp">
-      <span>
-        <h2>
-          <i>{task.title}</i>
-        </h2>
-        <p className="desc">{task.description}</p>
-        <span className="date">{task.date}</span>
-        {showAttendees && <Attendees people={task.rsvps} />}
-      </span>
+      <h2>
+        <i>{task.title}</i>
+      </h2>
+      <p className="desc">{task.description}</p>
+      <span className="date">{task.date}</span>
+      {showAttendees && <Attendees people={task.rsvps} />}
       <button onClick={handleAttendees}>
         {showAttendees ? "Close" : "Rsvps"}
       </button>
@@ -110,30 +131,135 @@ function RSVP({ task }) {
 
 function Attendees({ people }) {
   return (
-    <div>
+    <ul>
       {people.map((attend) => (
         <li key={attend.user}>
           {attend.user} | {attend.status}
         </li>
       ))}
+    </ul>
+  );
+}
+
+function AddEvent({ newEvent, showForm }) {
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [eventDate, setEventDate] = useState("");
+  function handleForm(e) {
+    e.preventDefault();
+    const newItem = {
+      id: v4(),
+      title: eventTitle,
+      description: eventDescription,
+      date: eventDate,
+      rsvps: [],
+    };
+    console.log(newItem);
+    newEvent(newItem);
+    setEventDate("");
+    setEventDescription("");
+    setEventTitle("");
+    showForm(false);
+  }
+
+  return (
+    <div>
+      <form className="event-adder" onSubmit={handleForm}>
+        <label>Event - Title : </label>
+        <input
+          type="text"
+          value={eventTitle}
+          onChange={(e) => setEventTitle(e.target.value)}
+        />
+
+        <label>Event - Description: </label>
+        <input
+          type="text"
+          value={eventDescription}
+          onChange={(e) => setEventDescription(e.target.value)}
+        />
+
+        <label>Event - Date</label>
+        <input
+          type="date"
+          value={eventDate}
+          onChange={(e) => setEventDate(e.target.value)}
+        />
+        <button style={{ margin: 10 }}>Submit</button>
+      </form>
     </div>
   );
 }
 
-function AddEvent() {
+function AddRSVP({ showRSVP, rsvp }) {
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [event, setEvent] = useState("");
+
+  console.log(event);
+
+  function addMembers(e) {
+    e.preventDefault();
+    const newUser = {
+      user: name,
+      status: status,
+    };
+
+    rsvp.filter((eve) =>
+      eve.title === event
+        ? (eve.rsvps = [...eve.rsvps, newUser])
+        : console.log("Not-Done")
+    );
+
+    console.log(newUser);
+    setName("");
+    setStatus("");
+    showRSVP(false);
+  }
+
   return (
     <div>
-      <form className="event-adder">
-        <label>Event - Title : </label>
-        <input type="text" />
+      <form className="event-adder" onSubmit={addMembers}>
+        <label>RSVP : </label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <label>Event - description: </label>
-        <input type="text" />
+        <label>Status : </label>
+        <input
+          type="text"
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        />
 
-        <label>Event - Date</label>
-        <input type="date" />
+        <select
+          style={{
+            backgroundColor: "#B3E2A7",
+            height: "20px",
+            margin: ".5rem",
+            borderRadius: "50px",
+          }}
+          value={event.title}
+          onChange={(e) => setEvent(e.target.value)}
+        >
+          {rsvp.map((event) => (
+            <option>{event.title}</option>
+          ))}
+        </select>
+
         <button style={{ margin: 10 }}>Submit</button>
       </form>
+    </div>
+  );
+}
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <h3 style={{ marginTop: "35px" }}>Copyright to 🧑‍💻 @Abhik4004 - {year}</h3>
     </div>
   );
 }
